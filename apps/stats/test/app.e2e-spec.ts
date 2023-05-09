40eb803a2ffe12743badf38b10b8542de188421a
Vahid V <vhid.vz@gmail.com>
2023-05-09 11:45:18 +0330

    update

--- apps/stats/test/app.e2e-spec.ts
+++ apps/stats/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { StatsModule } from './../src/stats.module';
+import { StatsModule } from '../src/stats.module';
 
 describe('StatsController (e2e)', () => {
   let app: INestApplication;
