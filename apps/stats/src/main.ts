e17ed90393826ad136d343039ce8c25920b9fb5e
Vahid V <vhid.vz@gmail.com>
2023-05-09 18:53:14 +0330

    update

--- apps/stats/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { StatsModule } from './stats.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(StatsModule);
-  await app.listen(3000);
-}
-bootstrap();
