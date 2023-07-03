c439ec6543a8344d3a38a3fe5914a60730651ad3
Vahid V <vhid.vz@gmail.com>
2023-07-03 17:28:20 +0330

    update

--- jest.config.ts
+++ jest.config.ts
@@ -11,6 +11,18 @@ const config: Config = {
   coverageDirectory: './coverage',
   testEnvironment: 'node',
   roots: ['<rootDir>/apps/'],
+  moduleNameMapper: {
+    '@app/common/(.*)': '<rootDir>/libs/common/src/$1',
+    '@app/common': '<rootDir>/libs/common/src',
+    '@app/command/(.*)': '<rootDir>/libs/command/src/$1',
+    '@app/command': '<rootDir>/libs/command/src',
+    '@app/blacklisted/(.*)': '<rootDir>/libs/blacklisted/src/$1',
+    '@app/blacklisted': '<rootDir>/libs/blacklisted/src',
+    '@app/health/(.*)': '<rootDir>/libs/health/src/$1',
+    '@app/health': '<rootDir>/libs/health/src',
+    '@app/redis/(.*)': '<rootDir>/libs/redis/src/$1',
+    '@app/redis': '<rootDir>/libs/redis/src',
+  },
 };
 
 export default config;
