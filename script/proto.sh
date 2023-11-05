8c2d32320614c6fcc617f086438c32c72ddc6869
Vahid V. <vhid.vz@gmail.com>
2023-11-05 13:57:18 +0330

    update

--- script/proto.sh
+++ script/proto.sh
@@ -3,8 +3,13 @@
 declare -A PROTOS
 
 # Protos
+CONFIG=./proto/config.proto
 DOMAIN=./proto/domain.proto
 IDENTITY=./proto/identity.proto
+
+# Config Proto
+PROTOS[CONFIG,1]=./apps/services/config/src/app.proto
+PROTOS[CONFIG,2]=./apps/gateway/src/modules/config/config.proto
 
 # Domain Proto
 PROTOS[DOMAIN,1]=./apps/services/domain/src/app.proto
@@ -16,6 +21,9 @@ PROTOS[IDENTITY,2]=./apps/gateway/src/modules/identity/identity.proto
 
 # Main Program
 for KEY in "${!PROTOS[@]}"; do
+  if [[ $KEY == *"CONFIG"* ]]; then
+    if ! test -f ${PROTOS[$KEY]}; then ln $CONFIG ${PROTOS[$KEY]}; fi
+  fi
   if [[ $KEY == *"DOMAIN"* ]]; then
     if ! test -f ${PROTOS[$KEY]}; then ln $DOMAIN ${PROTOS[$KEY]}; fi
   fi
