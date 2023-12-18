70d90d3b1d69b541b756064738a28522eafae543
Vahid V. <vhid.vz@gmail.com>
2023-12-18 10:35:01 +0330

    update

--- apps/gateway/src/modules/special/crafts/files/arts/private/private.controller.ts
+++ apps/gateway/src/modules/special/crafts/files/arts/private/private.controller.ts
@@ -3,28 +3,40 @@ import {
   Post,
   UploadedFiles,
   UseFilters,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { GatewayInterceptors, WriteInterceptors } from '@app/common/interceptors';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { SetPolicy, SetScope, ShipStrategy } from '@app/common/metadatas';
+import { AuthGuard, PolicyGuard, ScopeGuard } from '@app/common/guards';
+import { Action, Resource, Scope } from '@app/common/enums';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { SentryInterceptor } from '@ntegral/nestjs-sentry';
 import { AllExceptionsFilter } from '@app/common/filters';
 import { FileSerializer } from '@app/common/serializers';
 import { SpecialProvider } from '@app/common/providers';
+import { Meta, Session } from '@app/common/decorators';
 import { Metadata } from '@app/common/interfaces';
 import { CreateFileDto } from '@app/common/dto';
-import { Meta } from '@app/common/decorators';
+import { ClientSession } from 'mongoose';
 
+@ApiBearerAuth()
 @ApiTags('files')
 @Controller('files')
 @UseFilters(AllExceptionsFilter)
-@UseInterceptors(new SentryInterceptor())
+@UseGuards(AuthGuard, ScopeGuard, PolicyGuard)
+@UseInterceptors(...GatewayInterceptors, new SentryInterceptor())
 export class PrivateController {
   constructor(readonly provider: SpecialProvider) {}
 
   @Post('upload/private')
+  @ShipStrategy('create')
+  @SetScope(Scope.UploadSpecialFiles)
   @ApiConsumes('multipart/form-data')
+  @UseInterceptors(...WriteInterceptors)
   @UseInterceptors(FilesInterceptor('file'))
+  @SetPolicy(Action.Upload, Resource.SpecialFiles)
   @ApiBody({
     schema: {
       type: 'object',
@@ -36,7 +48,8 @@ export class PrivateController {
   upload(
     @Meta() meta: Metadata,
     @UploadedFiles() files: CreateFileDto[],
+    @Session() session?: ClientSession,
   ): Promise<FileSerializer[]> {
-    return this.provider.files.upload(files, { meta });
+    return this.provider.files.upload(files, { meta, session });
   }
 }
