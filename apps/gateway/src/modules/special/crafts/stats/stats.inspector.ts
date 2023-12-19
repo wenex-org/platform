997b7579073a23609607389b7307da7ec152c627
Vahid V. <vhid.vz@gmail.com>
2023-12-19 12:58:24 +0330

    update

---
+++ apps/gateway/src/modules/special/crafts/stats/stats.inspector.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UseFilters,
+  UseGuards,
+  UseInterceptors,
+  UsePipes,
+} from '@nestjs/common';
+import { GatewayInterceptors, WriteInterceptors } from '@app/common/interceptors';
+import { SetPolicy, SetScope, ShipStrategy } from '@app/common/metadatas';
+import { AuthGuard, PolicyGuard, ScopeGuard } from '@app/common/guards';
+import { Action, Resource, Scope } from '@app/common/enums';
+import { SentryInterceptor } from '@ntegral/nestjs-sentry';
+import { AllExceptionsFilter } from '@app/common/filters';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { SpecialProvider } from '@app/common/providers';
+import { ValidationPipe } from '@app/common/pipes';
+import { Metadata } from '@app/common/interfaces';
+import { CreateStatDto } from '@app/common/dto';
+import { Meta } from '@app/common/decorators';
+
+@ApiBearerAuth()
+@ApiTags('stats')
+@Controller('stats')
+@UsePipes(ValidationPipe)
+@UseFilters(AllExceptionsFilter)
+@UseGuards(AuthGuard, ScopeGuard, PolicyGuard)
+@UseInterceptors(...GatewayInterceptors, new SentryInterceptor())
+export class StatsInspector {
+  constructor(readonly provider: SpecialProvider) {}
+
+  @Post('collect')
+  @ShipStrategy('create')
+  @SetScope(Scope.CollectSpecialStats)
+  @UseInterceptors(...WriteInterceptors)
+  @SetPolicy(Action.Collect, Resource.SpecialStats)
+  async collet(@Meta() meta: Metadata, @Body() data: CreateStatDto) {
+    return this.provider.stats.collect(data, { meta });
+  }
+}
--- apps/gateway/src/modules/special/crafts/stats/stats.module.ts
+++ apps/gateway/src/modules/special/crafts/stats/stats.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { StatsResolver } from './stats.resolver';
+import { StatsInspector } from './stats.inspector';
 import { StatsController } from './stats.controller';
 
 @Module({
-  controllers: [StatsController],
+  controllers: [StatsController, StatsInspector],
   providers: [StatsResolver],
 })
 export class StatsModule {}
