852c767888a6b81b0813c63223754e2418e18e31
Vahid V. <vhid.vz@gmail.com>
2023-12-23 16:41:13 +0330

    update

---
+++ apps/gateway/src/modules/touch/crafts/mails/mails.inspector.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UseFilters,
+  UseGuards,
+  UseInterceptors,
+  UsePipes,
+} from '@nestjs/common';
+import { GatewayInterceptors, WriteInterceptors } from '@app/common/interceptors';
+import { SetPolicy, SetScope, ShipStrategy } from '@app/common/metadatas';
+import { AuthGuard, PolicyGuard, ScopeGuard } from '@app/common/guards';
+import { Action, Resource, Scope } from '@app/common/enums';
+import { SentryInterceptor } from '@ntegral/nestjs-sentry';
+import { AllExceptionsFilter } from '@app/common/filters';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { TouchProvider } from '@app/common/providers';
+import { ValidationPipe } from '@app/common/pipes';
+import { Metadata } from '@app/common/interfaces';
+import { CreateMailDto } from '@app/common/dto';
+import { Meta } from '@app/common/decorators';
+
+@ApiBearerAuth()
+@ApiTags('mails')
+@Controller('mails')
+@UsePipes(ValidationPipe)
+@UseFilters(AllExceptionsFilter)
+@UseGuards(AuthGuard, ScopeGuard, PolicyGuard)
+@UseInterceptors(...GatewayInterceptors, new SentryInterceptor())
+export class MailsInspector {
+  constructor(readonly provider: TouchProvider) {}
+
+  @Post('send')
+  @ShipStrategy('create')
+  @SetScope(Scope.SendTouchMails)
+  @UseInterceptors(...WriteInterceptors)
+  @SetPolicy(Action.Collect, Resource.TouchMails)
+  async send(@Meta() meta: Metadata, @Body() data: CreateMailDto) {
+    return this.provider.mails.send(data, { meta });
+  }
+}
--- apps/gateway/src/modules/touch/crafts/mails/mails.module.ts
+++ apps/gateway/src/modules/touch/crafts/mails/mails.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { MailsResolver } from './mails.resolver';
+import { MailsInspector } from './mails.inspector';
 import { MailsController } from './mails.controller';
 
 @Module({
-  controllers: [MailsController],
+  controllers: [MailsController, MailsInspector],
   providers: [MailsResolver],
 })
 export class MailsModule {}
