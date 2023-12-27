d21cf1b2b2f6a377b682837573fe81f85d92c415
Vahid V. <vhid.vz@gmail.com>
2023-12-27 10:05:15 +0330

    update

--- script/kafka-connect.sh
+++ script/kafka-connect.sh
@@ -3,11 +3,11 @@ if [ -f .env ]; then
 fi
 
 curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8082/connectors/ -d "{
-  "name": "mongo-source",
-  "config": {
-    "connector.class": "io.debezium.connector.mongodb.MongoDbConnector",
-    "mongodb.connection.string": "mongodb://$MONGO_USER:$MONGO_PASS@$MONGO_HOST/?$MONGO_QUERY",
-    "topic.prefix": "mongo",
-    "collection.include.list": "$MONGO_DB.\*"
+  \"name\": \"mongo-source\",
+  \"config\": {
+    \"connector.class\": \"io.debezium.connector.mongodb.MongoDbConnector\",
+    \"mongodb.connection.string\": \"mongodb://$MONGO_USER:$MONGO_PASS@$MONGO_HOST/?$MONGO_QUERY\",
+    \"topic.prefix\": \"mongo\",
+    \"collection.include.list\": \"$MONGO_DB.*\"
   }
 }"
