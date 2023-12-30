b06172840547f57c072bd0066782c2b5c756ee83
Vahid V. <vhid.vz@gmail.com>
2023-12-30 11:14:24 +0330

    update

--- script/proto.sh
+++ script/proto.sh
@@ -8,6 +8,8 @@ CONFIG=./proto/config.proto
 DOMAIN=./proto/domain.proto
 IDENTITY=./proto/identity.proto
 
+PRESERVER=./proto/workers/preserver.proto
+
 # Auth Proto
 PROTOS[AUTH,1]=./apps/services/auth/src/app.proto
 PROTOS[AUTH,2]=./apps/gateway/src/modules/auth/auth.proto
@@ -28,6 +30,9 @@ PROTOS[IDENTITY,1]=./apps/services/identity/src/app.proto
 PROTOS[IDENTITY,2]=./apps/services/auth/src/protobuf/identity.proto
 PROTOS[IDENTITY,3]=./apps/gateway/src/modules/identity/identity.proto
 
+# Preserver Proto
+PROTOS[PRESERVER,1]=./apps/workers/preserver/src/app.proto
+
 # Main Program
 for KEY in "${!PROTOS[@]}"; do
   if [[ $KEY == *"AUTH"* ]]; then
@@ -41,5 +46,8 @@ for KEY in "${!PROTOS[@]}"; do
   fi
   if [[ $KEY == *"IDENTITY"* ]]; then
     if ! test -f ${PROTOS[$KEY]}; then ln -f $IDENTITY ${PROTOS[$KEY]}; fi
+  fi
+  if [[ $KEY == *"PRESERVER"* ]]; then
+    if ! test -f ${PROTOS[$KEY]}; then ln -f $PRESERVER ${PROTOS[$KEY]}; fi
   fi
 done
