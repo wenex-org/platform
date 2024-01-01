434689ef6d1fe35d2d2c126055d57e9928048334
Vahid V. <vhid.vz@gmail.com>
2024-01-01 08:41:50 +0330

    update

--- script/setup.sh
+++ script/setup.sh
@@ -8,3 +8,6 @@
 sh ./script/proto.sh
 if [ $? == 0 ]; then echo -e "Proto prepared successfully.\n\n"
 else echo -e "\n\nPreparing proto assets was failed...!" && exit 1; fi
+
+# Kafka Connect
+echo -e "\nIf you want to setup kafka connect needs to run 'npm run script:kafka-connect'\n"
