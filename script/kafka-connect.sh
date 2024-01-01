d70760a07087e530614790ddb2fcd8d3cea77136
Vahid V. <vhid.vz@gmail.com>
2024-01-01 08:38:23 +0330

    update

--- script/kafka-connect.sh
+++ script/kafka-connect.sh
@@ -2,8 +2,17 @@ if [ -f .env ]; then
   export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
 fi
 
+MONGO_SOURCE="mongo-source"
+
+MONGO_SOURCE_EXISTS=$(curl -s -o /dev/null -w "%{http_code}" localhost:8082/connectors/$MONGO_SOURCE)
+
+if [ $MONGO_SOURCE_EXISTS -eq 200 ]; then
+  curl -X DELETE localhost:8082/connectors/$MONGO_SOURCE
+fi
+
+
 curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8082/connectors/ -d "{
-  \"name\": \"mongo-source\",
+  \"name\": \"$MONGO_SOURCE\",
   \"config\": {
     \"connector.class\": \"io.debezium.connector.mongodb.MongoDbConnector\",
     \"mongodb.connection.string\": \"mongodb://$MONGO_USER:$MONGO_PASS@$MONGO_HOST/?$MONGO_QUERY\",
--- script/setup.sh
+++ script/setup.sh
@@ -7,9 +7,4 @@
 # Proto
 sh ./script/proto.sh
 if [ $? == 0 ]; then echo -e "Proto prepared successfully.\n\n"
-else echo -e "\n\nPreparing proto assets was failed...!"; fi
-
-# Kafka Connect
-sh ./script/kafka-connect.sh
-if [ $? == 0 ]; then echo -e "\n\nKafka-Connect prepared successfully."
-else echo -e "\n\nPreparing Kafka-Connect was failed...!"; fi
+else echo -e "\n\nPreparing proto assets was failed...!" && exit 1; fi
