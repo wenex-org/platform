70daf9242ed4e43ef1598727af656d6b14756714
Vahid V. <vhid.vz@gmail.com>
2024-07-17 10:50:02 +0330

    update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@
 ######################
 
 # Proto
-bash ./script/proto.sh
+bash ./scripts/proto.sh
 if [ $? == 0 ]; then echo -e "Proto prepared successfully.\n\n"
 else echo -e "\n\nPreparing proto assets was failed...!" && exit 1; fi
 
