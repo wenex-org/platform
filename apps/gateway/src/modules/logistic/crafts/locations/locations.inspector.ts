842e6761319e8bc1f3c08c62f7c03530ffd9f233
Vahid V. <vhid.vz@gmail.com>
2024-09-06 18:55:20 +0330

    update

---
+++ apps/gateway/src/modules/logistic/crafts/locations/locations.inspector.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Post, UseFilters, UseGuards, UseInterceptors, UsePipes } from '@nestjs/common';
+import { FieldInterceptor, FilterInterceptor, GatewayInterceptors } from '@app/common/interceptors';
+import { BoundaryAdministrativeSerializer } from '@app/common/serializers';
+import { AuthGuard, PolicyGuard, ScopeGuard } from '@app/common/guards';
+import { Action, Resource, Scope } from '@app/common/enums';
+import { SetPolicy, SetScope } from '@app/common/metadatas';
+import { SentryInterceptor } from '@ntegral/nestjs-sentry';
+import { AllExceptionsFilter } from '@app/common/filters';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { LogisticProvider } from '@app/common/providers';
+import { ValidationPipe } from '@app/common/pipes';
+import { Metadata } from '@app/common/interfaces';
+import { Meta } from '@app/common/decorators';
+import { LatLngDto } from '@app/common/dto';
+
+@ApiBearerAuth()
+@ApiTags('locations')
+@Controller('locations')
+@UsePipes(ValidationPipe)
+@UseFilters(AllExceptionsFilter)
+@UseGuards(AuthGuard, ScopeGuard, PolicyGuard)
+@UseInterceptors(...GatewayInterceptors, new SentryInterceptor())
+export class LocationsInspector {
+  constructor(readonly provider: LogisticProvider) { }
+
+  @Post('location-to-address')
+  @SetScope(Scope.ResolveLogisticLocations)
+  @SetPolicy(Action.Send, Resource.LogisticLocations)
+  @UseInterceptors(FieldInterceptor, FilterInterceptor)
+  async locationToAddress(@Meta() meta: Metadata, @Body() data: LatLngDto): Promise<BoundaryAdministrativeSerializer> {
+    return this.provider.locations.locationToAddress(data, meta);
+  }
+}
--- apps/gateway/src/modules/logistic/crafts/locations/locations.module.ts
+++ apps/gateway/src/modules/logistic/crafts/locations/locations.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { LocationsResolver } from './locations.resolver';
+import { LocationsInspector } from './locations.inspector';
 import { LocationsController } from './locations.controller';
 
 @Module({
-  controllers: [LocationsController],
+  controllers: [LocationsController, LocationsInspector],
   providers: [LocationsResolver],
 })
-export class LocationsModule {}
+export class LocationsModule { }
