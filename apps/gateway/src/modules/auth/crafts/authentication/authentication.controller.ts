4883f783c704f433ec7dbc1def57bf2c1f2a9445
Vahid V. <vhid.vz@gmail.com>
2024-09-28 16:51:37 +0330

    update

--- apps/gateway/src/modules/auth/crafts/authentication/authentication.controller.ts
+++ apps/gateway/src/modules/auth/crafts/authentication/authentication.controller.ts
@@ -2,8 +2,6 @@ import { AuthenticationResponseSerializer, ResultSerializer, JwtTokenSerializer 
 import { Body, Controller, Post, UseFilters, UseGuards, UseInterceptors, UsePipes } from '@nestjs/common';
 import { AuthenticationRequestDto, TokenDto } from '@app/common/dto';
 import { GatewayInterceptors } from '@app/common/interceptors';
-import { AuthGuard, ScopeGuard } from '@app/common/guards';
-import { IsPublic, SetScope } from '@app/common/metadatas';
 import { SentryInterceptor } from '@ntegral/nestjs-sentry';
 import { AllExceptionsFilter } from '@app/common/filters';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
@@ -11,18 +9,19 @@ import { AuthProvider } from '@app/common/providers';
 import { ValidationPipe } from '@app/common/pipes';
 import { mapToInstance } from '@app/common/utils';
 import { Metadata } from '@app/common/interfaces';
+import { IsPublic } from '@app/common/metadatas';
+import { AuthGuard } from '@app/common/guards';
 import { Meta } from '@app/common/decorators';
-import { Scope } from '@app/common/enums';
 import { Observable, from } from 'rxjs';
 
 @ApiTags('auth')
 @Controller('auth')
+@UseGuards(AuthGuard)
 @UsePipes(ValidationPipe)
 @UseFilters(AllExceptionsFilter)
-@UseGuards(AuthGuard, ScopeGuard)
 @UseInterceptors(...GatewayInterceptors, new SentryInterceptor())
 export class AuthenticationController {
-  constructor(readonly provider: AuthProvider) {}
+  constructor(readonly provider: AuthProvider) { }
 
   @IsPublic()
   @Post('token')
@@ -32,14 +31,12 @@ export class AuthenticationController {
 
   @Post('verify')
   @ApiBearerAuth()
-  @SetScope(Scope.ManageAuth)
   verify(@Meta() meta: Metadata, @Body() data: TokenDto): Observable<JwtTokenSerializer> {
     return from(this.provider.authentication.verify(data, meta)).pipe(mapToInstance(JwtTokenSerializer));
   }
 
   @Post('logout')
   @ApiBearerAuth()
-  @SetScope(Scope.ManageAuth)
   logout(@Meta() meta: Metadata, @Body() data: TokenDto): Observable<ResultSerializer> {
     return from(this.provider.authentication.logout(data, meta)).pipe(mapToInstance(ResultSerializer));
   }
