66c703ec4169cad76fca0a914b89016af8fae961
Vahid V. <vhid.vz@gmail.com>
2024-10-14 13:30:21 +0330

    update

--- apps/gateway/src/modules/special/crafts/sagas/sagas.module.ts
+++ apps/gateway/src/modules/special/crafts/sagas/sagas.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { SagasResolver } from './sagas.resolver';
+import { SagasInspector } from './sagas.inspector';
 import { SagasController } from './sagas.controller';
 import { SagaHistoriesModule } from './subs/histories';
 
 @Module({
   imports: [SagaHistoriesModule],
-  controllers: [SagasController],
+  controllers: [SagasController, SagasInspector],
   providers: [SagasResolver],
 })
 export class SagasModule {}
