99fc09bb4a5a79a85e6dc2cf9a16e0afd5942b17
Vahid V. <vhid.vz@gmail.com>
2024-12-04 15:08:32 +0330

    update

--- apps/services/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { ServicesModule } from './services.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(ServicesModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
--- apps/services/src/services.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ServicesService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
