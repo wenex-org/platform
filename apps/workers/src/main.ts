6690c054d7cc509aeca70a2dd7ed08456a5b0b8a
Vahid V. <vhid.vz@gmail.com>
2024-12-08 08:10:30 +0330

    update

--- apps/workers/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { WorkersModule } from './workers.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(WorkersModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
