0ddf76cb0c27613ba3c0490a41101dc80a41bce0
Vahid V. <vhid.vz@gmail.com>
2024-12-14 16:59:47 +0330

    update

--- apps/gateway/test/app.e2e-spec.ts
+++ apps/gateway/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { GatewayModule } from './../src/gateway.module';
+import request from 'supertest';
+import { AppModule } from './../src/app.module';
 
 describe('GatewayController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [GatewayModule],
+      imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
