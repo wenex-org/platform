2c39b329ded471c9973bde39f405886aa8f1da39
Vahid V. <vhid.vz@gmail.com>
2024-12-15 11:08:56 +0330

    update

--- apps/gateway/test/app.e2e-spec.ts
+++ apps/gateway/test/app.e2e-spec.ts
@@ -6,7 +6,7 @@ import { AppModule } from './../src/app.module';
 describe('GatewayController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,7 +15,11 @@ describe('GatewayController (e2e)', () => {
     await app.init();
   });
 
+  afterAll(async () => {
+    await app.close();
+  });
+
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(404);
   });
 });
--- scripts/proto.sh
+++ scripts/proto.sh
@@ -9,10 +9,12 @@ DOMAIN=./protos/domain.proto
 # Auth Proto
 PROTOS[AUTH,1]=./apps/services/auth/src/app.proto
 PROTOS[AUTH,2]=./apps/gateway/src/protobuf/auth.proto
+PROTOS[AUTH,3]=./libs/common/src/providers/auth/protobuf/auth.proto
 
 # Domain Proto
 PROTOS[DOMAIN,1]=./apps/services/domain/src/app.proto
 PROTOS[DOMAIN,2]=./apps/gateway/src/protobuf/domain.proto
+PROTOS[DOMAIN,3]=./libs/common/src/providers/domain/protobuf/domain.proto
 
 # Main Program
 for KEY in "${!PROTOS[@]}"; do
