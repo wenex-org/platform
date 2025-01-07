497de62708ef4be13652adfd54ead70823c2800e
Vahid V. <vhid.vz@gmail.com>
2025-01-07 11:35:51 +0330

    update

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -14,7 +14,7 @@ import { NestFactory } from '@nestjs/core';
 import { APP } from '@app/common/core';
 import helmet from 'helmet';
 
-prototyping();
+prototyping('GATEWAY');
 import { AppModule } from './app.module';
 
 const { GATEWAY } = APP;
