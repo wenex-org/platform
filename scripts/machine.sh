d6118415d9a4bcd213d69cbba93f9dd562f0bd7b
Vahid V. <vhid.vz@gmail.com>
2025-01-29 13:49:39 +0330

    update

--- scripts/machine.sh
+++ scripts/machine.sh
@@ -7,12 +7,17 @@ ENV_FILE=".env"
 SECTION="# ----------------------------------------\n# AUTOMATICALLY GENERATED (DO NOT MODIFY)\n# ----------------------------------------"
 
 # Generate a random MACHINE_ID value
-RANDOM_ID=$(cat /dev/urandom | head -c 15 | base32)
+RANDOM_ID=$(head -c 15 /dev/urandom | base32)
 
 # Check if the file ends with the section and the MACHINE_ID variable
-if ! tail -n 4 "$ENV_FILE" 2>/dev/null | grep -q "MACHINE_ID="; then
-  echo -e "\n$SECTION\n\nMACHINE_ID=$RANDOM_ID" >> "$ENV_FILE"
-  echo "Section and MACHINE_ID added to the .env file."
+if ! tail -n 4 "$ENV_FILE" 2>/dev/null | grep -q "MACHINE_ID=[^ ]"; then
+  if grep -q "MACHINE_ID=$" "$ENV_FILE"; then
+    sed -i "s/MACHINE_ID=$/MACHINE_ID=$RANDOM_ID/" "$ENV_FILE"
+    echo "Empty MACHINE_ID filled in the .env file."
+  else
+    echo -e "\n$SECTION\n\nMACHINE_ID=$RANDOM_ID" >> "$ENV_FILE"
+    echo "Section and MACHINE_ID added to the .env file."
+  fi
 else
   echo "The section with MACHINE_ID already exists at the end of the file."
 fi
