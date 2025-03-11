bf259ec02504a9cc37ac29e306f0c2bae71a2b24
Vahid V. <vhid.vz@gmail.com>
2025-03-11 08:32:38 +0330

    update

--- scripts/git-submodule.sh
+++ scripts/git-submodule.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git submodule foreach git $1 $2 $3 $4 $5 $6 $7 $8 $9
-git $1 $2 $3 $4 $5 $6 $7 $8 $9
+git submodule foreach git $@
+git $@
 
 if [ $1 = "flow" ] && { [ $2 = "release" ] || [ $2 = "hotfix" ]; } && [ $3 = "finish" ]; then
   git submodule foreach git push origin --tags
--- scripts/start.sh
+++ scripts/start.sh
@@ -3,12 +3,12 @@
 SERVICE_NAME=${1:-gateway}
 
 if [ $SERVICE_NAME == "gateway" ]; then
-  node dist/apps/gateway/main.js
+  node --stack-size=6144 dist/apps/gateway/main.js
 else
   if [ -d "dist/apps/services/$SERVICE_NAME" ]; then
-    node dist/apps/services/$SERVICE_NAME/main.js
+    node --stack-size=6144 dist/apps/services/$SERVICE_NAME/main.js
   elif [ -d "dist/apps/workers/$SERVICE_NAME" ]; then
-    node dist/apps/workers/$SERVICE_NAME/main.js
+    node --stack-size=6144 dist/apps/workers/$SERVICE_NAME/main.js
   else
     echo -e "Service or Worker not found...!\n" && exit 1
   fi
