06db7b198f448a0788c91cd45a4f8716338e8b8d
Vahid V. <vhid.vz@gmail.com>
2025-06-21 17:33:19 +0330

    update

--- scripts/end-2-end-start.sh
+++ scripts/end-2-end-start.sh
@@ -1,61 +1,30 @@
 #!/bin/bash
 
 mkdir -p logs
-declare -A JOBS
 
-npm run db:clean:e2e && npm run db:index:e2e && npm run db:seed:e2e
+shell=${SHELL:-/bin/bash}
+if [ -z "$shell" ]; then
+  echo "ERROR: No suitable shell found. Please install zsh or bash."
+  exit 1
+fi
 
-# Start services and capture PIDs
-services=("auth" "context" "domain" "essential")
+terminal=$(command -v kgx)
+if [ -z "$terminal" ]; then
+  terminal=$(command -v gnome-terminal)
+  if [ -z "$terminal" ]; then
+    echo "WARN: No suitable terminal found. Please install kgx or gnome-terminal."
+  fi
+fi
+
+services=($(echo $@ | tr ',' ' '))
+
 for service in "${services[@]}"; do
-  npm run start:dev:e2e "$service" 2> "logs/$service.log" &
-  JOBS[$service]=$!
-  echo "Started $service with PID: ${JOBS[$service]}"
+  if [ -z "$terminal" ]; then
+    npm run start:dev:e2e "$service" 2> "logs/$service.log" &
+  else
+    $terminal --tab --title="$service" -- $shell -c "npm run start:dev:e2e $service" &
+  fi
+  sleep 30
 done
 
-echo "Waiting for services to start..."
-sleep 90
-
-services=("financial" "general" "identity" "special")
-for service in "${services[@]}"; do
-  npm run start:dev:e2e "$service" 2> "logs/$service.log" &
-  JOBS[$service]=$!
-  echo "Started $service with PID: ${JOBS[$service]}"
-done
-
-echo "Waiting for services to start..."
-sleep 90
-
-services=("touch" "content" "logistic" "conjoint")
-for service in "${services[@]}"; do
-  npm run start:dev:e2e "$service" 2> "logs/$service.log" &
-  JOBS[$service]=$!
-  echo "Started $service with PID: ${JOBS[$service]}"
-done
-
-echo "Waiting for services to start..."
-sleep 90
-
-services=("gateway")
-for service in "${services[@]}"; do
-  npm run start:dev:e2e "$service" 2> "logs/$service.log" &
-  JOBS[$service]=$!
-  echo "Started $service with PID: ${JOBS[$service]}"
-done
-
-echo "Waiting for services to start..."
-sleep 90
-
-npm run script:kafka-connect:e2e
-
-# Start additional services
-additional_services=("watcher" "preserver" "observer" "dispatcher")
-for service in "${additional_services[@]}"; do
-  npm run start:dev:e2e "$service" 2> "logs/$service.log" &
-  JOBS[$service]=$!
-  echo "Started $service with PID: ${JOBS[$service]}"
-done
-
-echo "Waiting for workers to start..."
-sleep 90
 echo "All services started successfully!"
