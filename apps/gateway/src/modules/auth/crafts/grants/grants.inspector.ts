67aeeedc2072b529af8986e507bf927752280fab
fdaei <firoozedaeizadeh@gmail.com>
2025-10-26 14:44:43 +0330

    update

--- apps/gateway/src/modules/auth/crafts/grants/grants.inspector.ts
+++ apps/gateway/src/modules/auth/crafts/grants/grants.inspector.ts
@@ -1,23 +1,33 @@
 import { ServerMCP } from '@app/common/core/mcp';
+import { QUERY_DECS } from '@app/common/core/mcp/descs.mcp';
+import { toString } from '@app/common/core/utils';
 
-export function mcpRegistration() {
+export async function mcpRegistration() {
   const mcp = ServerMCP.create();
+  const { ResourceTemplate } = await import('@modelcontextprotocol/sdk/server/mcp.js');
 
-  // count
   mcp.server.registerResource(
     'countAuthGrant',
-    'auth://grants/count',
+    new ResourceTemplate('auth://grants/count{?query}', {
+      list: () => ({
+        resources: [],
+      }),
+    }),
     {
       title: 'Application Config',
-      description: 'Application configuration data',
+      description: `Application configuration data query="${QUERY_DECS}"`,
     },
     (uri, extra) => {
       console.log('uri', uri, 'extra', extra);
+      const queryParam = uri.searchParams.get('query');
+      console.log(queryParam);
+
       return {
         contents: [
           {
             uri: uri.href,
-            text: 'App configuration here',
+            text: toString({}),
+            mimeType: 'application/json',
           },
         ],
       };
