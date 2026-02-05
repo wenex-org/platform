e56645bee7f02c68b1d75cf76bf5231c060a4574
Vahid V. <vhid.vz@gmail.com>
2026-02-05 13:44:58 +0330

    update

--- apps/gateway/src/modules/auth/crafts/grants/grants.inspector.ts
+++ apps/gateway/src/modules/auth/crafts/grants/grants.inspector.ts
@@ -1,29 +1,37 @@
-import { ServerMCP } from '@app/common/core/mcp';
+import { getHeaders, ServerMCP } from '@app/common/core/mcp';
+import { fixOut } from '@app/common/core/utils/mongo';
+import { Action, Resource } from '@app/common/core';
 import { z } from 'zod';
 
 export function mcpRegistration() {
   const mcp = ServerMCP.create();
-
-  // count
-  mcp.server.registerResource(
-    'countAuthGrant',
-    'auth://grants/count',
+  mcp.server.registerTool(
+    'create_auth_grant',
     {
-      title: 'Application Config',
-      description: 'Application configuration data',
-      _meta: {
-        filter: z.object({}).describe('Json mongodb query'),
+      title: 'Add a new Grant',
+      description: 'Create a new grant with subject, action and object',
+      inputSchema: {
+        subject: z.string().nonempty().email(),
+        action: z.nativeEnum(Action),
+        object: z.nativeEnum(Resource),
       },
     },
-    (uri, extra) => {
-      console.log('uri', uri, 'extra', extra);
+    async (data, { signal, requestInfo }) => {
+      const log = mcp.log.extend('create_auth_grant');
+
+      if (signal.aborted) throw new Error('tools/call was cancelled');
+
+      const headers = getHeaders({ requestInfo });
+      if (!headers.authorization) {
+        return { content: [{ type: 'text', text: 'authorization token not found' }] };
+      }
+
+      log('Trying to create grant...');
+      const grant = await mcp.platform.auth.grants.create(data, { headers });
+      log('Grant created with result: %o', grant);
+
       return {
-        contents: [
-          {
-            uri: uri.href,
-            text: 'App configuration here',
-          },
-        ],
+        content: [{ type: 'resource', mimeType: 'application/json', resource: fixOut(grant) }],
       };
     },
   );
